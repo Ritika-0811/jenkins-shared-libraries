8b79e66ddadef5a3332a4e5fedd4c3a0254b0be7
Ritika Varshney <varshneyritika08@gmail.com>
2026-03-30 13:05:48 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -3,5 +3,4 @@ def call(String Project, String ImageTag, String dockerHubUser){
   sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
 }
 sh "docker push ${dockerhubUser}/${Project}:${ImageTag}"
-}
 
