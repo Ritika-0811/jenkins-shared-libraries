1cb5711eb734c5544a4ab1e7e5d48d50bdd8d507
Ritika Varshney <varshneyritika08@gmail.com>
2026-03-30 13:08:32 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,6 +1,7 @@
 def call(String Project, String ImageTag, String dockerHubUser){
   withCredentials([usernamePassword(credentialsId: "dockerhubCred",passwordVariable: "dockerhubPass",usernameVariable: "dockerhubUser")])
-  sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
+  sh "docker login -u ${dockerhubUser} -p ${dockerhubPass}"{
+    sh "docker push ${dockerhubUser}/${Project}:${ImageTag}"
+  }
 }
-sh "docker push ${dockerhubUser}/${Project}:${ImageTag}"
 
